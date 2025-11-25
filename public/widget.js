773cbc18c05d33655444d68d7c7b7c714ec34ce4
jaswant-d4d <jaswant.d4d@gmail.com>
2025-11-25 16:28:56 +0530

    minor changes

--- public/widget.js
+++ public/widget.js
@@ -6,12 +6,14 @@
   iframe.style.position = "fixed";
   iframe.style.bottom = "20px";
   iframe.style.right = "20px";
-  iframe.style.width = "320px";
+  iframe.style.minWidth = "320px";
+  iframe.style.maxWidth = "380px";
   iframe.style.height = "650px";
   iframe.style.border = "none";
   iframe.style.zIndex = "999999999999999 !important";
   iframe.style.borderRadius = "16px";
   iframe.style.pointerEvents = "auto";
+  
 
   document.body.appendChild(iframe);
 })();
--- src/components/chatbox/index.tsx
+++ src/components/chatbox/index.tsx
@@ -32,9 +32,9 @@ const Chatbox = () => {
   if (!isChatVisible) return null;
 
   return (
-    <div className="fixed p-2 sm:p-0 bottom-0 right-0 sm:bottom-8 sm:right-8 h-[calc(100vh-62px)] sm:h-[80vh] z-50 transition-all ease-in-out duration-500 w-full sm:w-[calc(100vw-60px)]">
-      <div className=' w-full sm:w-sm rounded-2xl h-full bg-white ml-auto'>
-        <div className='flex flex-col h-full w-full sm:shadow-lg rounded-2xl relative overflow-hidden pb-14 pt-14 sm:pt-18'>
+    <div className="fixed sm:p-0 bottom-0 right-0 sm:bottom-8  sm:right-8 h-[calc(100vh-62px)] sm:h-[80vh] z-50 transition-all ease-in-out duration-500 w-full sm:w-[calc(100vw-60px)]">
+      <div className=' w-full sm:w-sm rounded-2xl h-full shadow-xl bg-white ml-auto'>
+        <div className='flex flex-col h-full w-full  rounded-2xl relative overflow-hidden pb-14 pt-14 sm:pt-18'>
 
           {!showContent ? (
             <div className="flex-1 flex items-center justify-center">
